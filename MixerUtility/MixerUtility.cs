d2d07698ad33ba0c1a496e20c861b27562dc9d37
Eg0 <gioeleriva@outlook.it>
2017-03-27 13:12:21 +0200

    First commit

---
+++ MixerUtility/MixerUtility.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.IO;
+using System.Collections.Generic;
+using System.Collections;
+using System.Management;
+using System.Diagnostics;
+using System.Drawing;
+using AudioSwitcher.AudioApi.CoreAudio;
+
+namespace MixerUtility
+{
+    class MixerUtility
+    {
+
+        static int Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                return 1;
+            }
+
+            if (args[0].Equals("getmixerapps"))
+            {
+                GetMixerApps();
+                return 1;
+            }
+            if (args[0].Equals("getappvolume"))
+            {
+                Console.WriteLine(GetApplicationVolume(int.Parse(args[1])));
+                return 1;
+            }
+            if (args[0].Equals("setappvolume"))
+            {
+                SetApplicationVolume(int.Parse(args[1]), float.Parse(args[2]));
+                return 1;
+            }
+            if (args[0].Equals("getsystemvolume"))
+            {
+                GetSystemVolume();
+                return 1;
+            }
+            if (args[0].Equals("setsystemvolume"))
+            {
+                SetSystemVolume(int.Parse(args[1]));
+                return 1;
+            }
+
+            return 0;
+        }
+
+        public static void GetSystemVolume()
+        {
+            CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+            Console.WriteLine(defaultPlaybackDevice.Volume);
+        }
+
+        public static void SetSystemVolume(int volume)
+        {
+            CoreAudioDevice defaultPlaybackDevice = new CoreAudioController().DefaultPlaybackDevice;
+            defaultPlaybackDevice.Volume = volume;
+        }
+
+
+        private static void SaveAppIcon(Process process)
+        {
+            String appPath = GetMainModuleFilepath(process.Id);
+            Icon icon = Icon.ExtractAssociatedIcon(appPath);
+            Bitmap bmp = icon.ToBitmap();
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            Directory.CreateDirectory(path + "\\MixerUtility");
+            path = path + "\\MixerUtility\\";
+            bmp.Save(path + process.ProcessName + ".bmp");
+        }
+
+
+        public static void GetMixerApps()
+        {
+            Process[] allProcesses = Process.GetProcesses();
+            ArrayList distinctMixerProcessesNames = new ArrayList();
+            List<Process> mixerProcesses = new List<Process>();
+
+            for (int i = 0; i < allProcesses.Length; i++)
+            {
+                if (!GetApplicationVolume(allProcesses[i].Id).Equals(null))
+                {
+                    if (!distinctMixerProcessesNames.Contains(allProcesses[i].ProcessName))
+                    {
+                        distinctMixerProcessesNames.Add(allProcesses[i].ProcessName);
+                        mixerProcesses.Add(Process.GetProcessById(allProcesses[i].Id));
+                    }
+                }
+            }
+            
+            for (int x = 0; x < mixerProcesses.Count - 1; x++)
+            {
+                string productName = mixerProcesses[x].ProcessName;
+                try
+                {
+                String appPath = GetMainModuleFilepath(mixerProcesses[x].Id);
+                FileVersionInfo fvi = FileVersionInfo.GetVersionInfo(appPath);
+                productName = fvi.ProductName;
+                SaveAppIcon(mixerProcesses[x]);
+                }
+                catch(Exception e)
+                {
+                }
+                Console.WriteLine(mixerProcesses[x].ProcessName + "#" + productName + "#" + mixerProcesses[x].Id + "#" + GetApplicationVolume(mixerProcesses[x].Id));
+            }
+        }
+
+        private static string GetMainModuleFilepath(int processId)
+        {
+            string wmiQueryString = "SELECT ProcessId, ExecutablePath FROM Win32_Process WHERE ProcessId = " + processId;
+            using (var searcher = new ManagementObjectSearcher(wmiQueryString))
+            {
+                using (var results = searcher.Get())
+                {
+                    ManagementObject mo = results.Cast<ManagementObject>().FirstOrDefault();
+                    if (mo != null)
+                    {
+                        return (string)mo["ExecutablePath"];
+                    }
+                }
+            }
+            return null;
+        }
+
+        public static float? GetApplicationVolume(int pid)
+        {
+            ISimpleAudioVolume volume = GetVolumeObject(pid);
+            if (volume == null)
+                return null;
+
+            float level;
+            volume.GetMasterVolume(out level);
+            Marshal.ReleaseComObject(volume);
+            return level * 100;
+        }
+
+        public static void SetApplicationVolume(int pid, float level)
+        {
+            ISimpleAudioVolume volume = GetVolumeObject(pid);
+            if (volume == null)
+                return;
+
+            Guid guid = Guid.Empty;
+            volume.SetMasterVolume(level / 100, ref guid);
+            Marshal.ReleaseComObject(volume);
+        }
+
+        private static ISimpleAudioVolume GetVolumeObject(int pid)
+        {
+            IMMDeviceEnumerator deviceEnumerator = (IMMDeviceEnumerator)(new MMDeviceEnumerator());
+            IMMDevice speakers;
+            deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia, out speakers);
+
+            Guid IID_IAudioSessionManager2 = typeof(IAudioSessionManager2).GUID;
+            object o;
+            speakers.Activate(ref IID_IAudioSessionManager2, 0, IntPtr.Zero, out o);
+            IAudioSessionManager2 mgr = (IAudioSessionManager2)o;
+
+            IAudioSessionEnumerator sessionEnumerator;
+            mgr.GetSessionEnumerator(out sessionEnumerator);
+            int count;
+            sessionEnumerator.GetCount(out count);
+
+            ISimpleAudioVolume volumeControl = null;
+            for (int i = 0; i < count; i++)
+            {
+                IAudioSessionControl2 ctl;
+                sessionEnumerator.GetSession(i, out ctl);
+                int cpid;
+                ctl.GetProcessId(out cpid);
+
+                if (cpid == pid)
+                {
+                    volumeControl = ctl as ISimpleAudioVolume;
+                    break;
+                }
+                Marshal.ReleaseComObject(ctl);
+            }
+            Marshal.ReleaseComObject(sessionEnumerator);
+            Marshal.ReleaseComObject(mgr);
+            Marshal.ReleaseComObject(speakers);
+            Marshal.ReleaseComObject(deviceEnumerator);
+            return volumeControl;
+        }
+    }
+
+    [ComImport]
+    [Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")]
+    internal class MMDeviceEnumerator
+    {
+    }
+
+    internal enum EDataFlow
+    {
+        eRender,
+        eCapture,
+        eAll,
+        EDataFlow_enum_count
+    }
+
+    internal enum ERole
+    {
+        eConsole,
+        eMultimedia,
+        eCommunications,
+        ERole_enum_count
+    }
+
+    [Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IMMDeviceEnumerator
+    {
+        int NotImpl1();
+
+        [PreserveSig]
+        int GetDefaultAudioEndpoint(EDataFlow dataFlow, ERole role, out IMMDevice ppDevice);
+
+    }
+
+    [Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IMMDevice
+    {
+        [PreserveSig]
+        int Activate(ref Guid iid, int dwClsCtx, IntPtr pActivationParams, [MarshalAs(UnmanagedType.IUnknown)] out object ppInterface);
+
+    }
+
+    [Guid("77AA99A0-1BD6-484F-8BC7-2C654C9A9B6F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IAudioSessionManager2
+    {
+        int NotImpl1();
+        int NotImpl2();
+
+        [PreserveSig]
+        int GetSessionEnumerator(out IAudioSessionEnumerator SessionEnum);
+
+    }
+
+    [Guid("E2F5BB11-0570-40CA-ACDD-3AA01277DEE8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IAudioSessionEnumerator
+    {
+        [PreserveSig]
+        int GetCount(out int SessionCount);
+
+        [PreserveSig]
+        int GetSession(int SessionCount, out IAudioSessionControl2 Session);
+    }
+
+    [Guid("87CE5498-68D6-44E5-9215-6DA47EF883D8"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface ISimpleAudioVolume
+    {
+        [PreserveSig]
+        int SetMasterVolume(float fLevel, ref Guid EventContext);
+
+        [PreserveSig]
+        int GetMasterVolume(out float pfLevel);
+
+        [PreserveSig]
+        int SetMute(bool bMute, ref Guid EventContext);
+
+        [PreserveSig]
+        int GetMute(out bool pbMute);
+    }
+
+    [Guid("bfb7ff88-7239-4fc9-8fa2-07c950be9c6d"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IAudioSessionControl2
+    {
+        [PreserveSig]
+        int NotImpl0();
+
+        [PreserveSig]
+        int GetDisplayName([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
+
+        [PreserveSig]
+        int SetDisplayName([MarshalAs(UnmanagedType.LPWStr)]string Value, [MarshalAs(UnmanagedType.LPStruct)] Guid EventContext);
+
+        [PreserveSig]
+        int GetIconPath([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
+
+        [PreserveSig]
+        int SetIconPath([MarshalAs(UnmanagedType.LPWStr)] string Value, [MarshalAs(UnmanagedType.LPStruct)] Guid EventContext);
+
+        [PreserveSig]
+        int GetGroupingParam(out Guid pRetVal);
+
+        [PreserveSig]
+        int SetGroupingParam([MarshalAs(UnmanagedType.LPStruct)] Guid Override, [MarshalAs(UnmanagedType.LPStruct)] Guid EventContext);
+
+        [PreserveSig]
+        int NotImpl1();
+
+        [PreserveSig]
+        int NotImpl2();
+
+        [PreserveSig]
+        int GetSessionIdentifier([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
+
+        [PreserveSig]
+        int GetSessionInstanceIdentifier([MarshalAs(UnmanagedType.LPWStr)] out string pRetVal);
+
+        [PreserveSig]
+        int GetProcessId(out int pRetVal);
+
+        [PreserveSig]
+        int IsSystemSoundsSession();
+
+        [PreserveSig]
+        int SetDuckingPreference(bool optOut);
+    }
+}
